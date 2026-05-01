316ebfd88a3ef2954a8c4cd797c0ad9ac78086bb
Antoine Stevenson <antoine@kunming-afchine.org>
2026-05-01 18:56:38 +0200

    Niveaux A1-C2 : padding sections scopé aux 6 pages (plus homepage)

--- templates/shaper_languageschool/index.php
+++ templates/shaper_languageschool/index.php
@@ -138,7 +138,7 @@ if ($custom_js = $this->params->get('custom_js'))
 
 <!doctype html>
 <html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
-  <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/shaper_languageschool/css/mobile_cards.css?v=20260501d">  
+  <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/shaper_languageschool/css/mobile_cards.css?v=20260501e">  
   <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
